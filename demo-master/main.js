154b7ef50279da6869c2c2d9c2c360dc0e27d272
KaylaPretorius <38974317+KaylaPretorius@users.noreply.github.com>
2020-07-30 03:16:12 +0200

    Add files via upload

---
+++ demo-master/main.js
@@ -0,0 +1,6 @@
+
+// the onchange function will execute when a new value is selected by the user
+function myFunction() {
+var x = document.getElementById("type").value;
+document.getElementById("test").innerHTML = "You selected: " + x;
+}
